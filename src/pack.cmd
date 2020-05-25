b84e11139fed223e86ca423c404485868fec2626
thepirat000 <thepirat000@hotmail.com>
2020-05-25 13:48:05 -0500

    pack

--- src/pack.cmd
+++ src/pack.cmd
@@ -1,5 +1,7 @@
 del "CachingFramework.Redis\bin\debug\*.nupkg"
 del "CachingFramework.Redis.MsgPack\bin\debug\*.nupkg"
+del "CachingFramework.Redis.NewtonsoftJson\bin\debug\*.nupkg"
 
 dotnet pack "CachingFramework.Redis/"
 dotnet pack "CachingFramework.Redis.MsgPack/"
+dotnet pack "CachingFramework.Redis.NewtonsoftJson/"
