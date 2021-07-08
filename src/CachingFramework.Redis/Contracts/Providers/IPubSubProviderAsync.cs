a8ab9cbe39c3aa10ea46a100110b2a65df0fb2dc
ali joghataei <a.joghataei@gmail.com>
2021-07-09 01:39:17 +0430

    Add Async support for PubSubProvider (#64)
    * SerializersBasedOnRedisValue
    
    * Add Async Support For PubSubProvider
    
    * Add Async Support PubSubProvider
    
    Co-authored-by: xxxx <xxx>
    Co-authored-by: Federico Daniel Colombo <thepirat000@hotmail.com>

--- src/CachingFramework.Redis/Contracts/Providers/IPubSubProviderAsync.cs
+++ src/CachingFramework.Redis/Contracts/Providers/IPubSubProviderAsync.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CachingFramework.Redis.Contracts.Providers
 {
     /// <summary>
-    /// A typed PubSub Provider interface
+    /// A typed PubSub Provider Async interface
     /// </summary>
-    public interface IPubSubProvider : IPubSubProviderAsync
+    public interface IPubSubProviderAsync
     {
         /// <summary>
         /// Subscribes to a specified channel for a speficied type.
@@ -13,25 +14,25 @@ namespace CachingFramework.Redis.Contracts.Providers
         /// <typeparam name="T">The item type</typeparam>
         /// <param name="channel">The channel name.</param>
         /// <param name="action">The action where the first parameter is the channel name and the second is the object message.</param>
-        void Subscribe<T>(string channel, Action<string, T> action);
+        Task SubscribeAsync<T>(string channel, Action<string, T> action);
         /// <summary>
         /// Subscribes to a specified channel for a speficied type.
         /// </summary>
         /// <typeparam name="T">The item type</typeparam>
         /// <param name="channel">The channel name.</param>
         /// <param name="action">The action where the first parameter is the object message.</param>
-        void Subscribe<T>(string channel, Action<T> action);
+        Task SubscribeAsync<T>(string channel, Action<T> action);
         /// <summary>
         /// Unsubscribes from the specified channel.
         /// </summary>
         /// <param name="channel">The channel name.</param>
-        void Unsubscribe(string channel);
+        Task UnsubscribeAsync(string channel);
         /// <summary>
         /// Publishes an object to the specified channel.
         /// </summary>
         /// <typeparam name="T">The type of item to publish</typeparam>
         /// <param name="channel">The channel name.</param>
         /// <param name="item">The item.</param>
-        void Publish<T>(string channel, T item);
+        Task PublishAsync<T>(string channel, T item);
     }
 }
